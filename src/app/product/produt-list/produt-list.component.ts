17d0b120b2711d96bfd6da42bac86bf2018a0b2b
IEVN1003-20001612 <20001612@alumnos.utleon.edu.mx>
2024-09-17 19:35:52 -0600

    17/09/2024 muestra imagen

---
+++ src/app/product/produt-list/produt-list.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produt-list',
+  templateUrl: './produt-list.component.html',
+  styleUrl: './produt-list.component.css'
+})
+export class ProdutListComponent {
+title="Saludo de variables"
+
+imageWidth:number=50;
+imageMargin:number=2;
+muestraImg:boolean=true;
+listFilter:string='';
+
+showImage():void{
+  this.muestraImg=!this.muestraImg;
+}
+
+productos:any[]=[
+  { 
+    "productoID":1,
+    "Modelo":'Sentra',
+    "Descripcion":"4 puertas familiar",
+    "Year":"febrero 3 2022",
+    "Precio":20000,
+    "Marca":"NISSAN",
+    "Color":"Morado",
+    "imagenUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN5KPNPdlYxa7jEBhYE68M7l3UOYEwO5C8rzfQ3m_ccONhN9gbwZ-Gn-DhNFKxAMyLdnc&usqp=CAU" 
+  },
+  {
+    "productoID":2,
+    "Modelo":'A4',
+    "Descripcion":"2 Puertas",
+    "Year":"Marzo 3 2022",
+    "Precio":30000,
+    "Marca":"AUDI",
+    "Color":"Blanco",
+    "imagenUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGn--Nso6OdOT0Fhs-9rAE0nffiO2A9Kz58STGIWVUUlw3OlWICROSxw_HidLX06csSHM&usqp=CAU" 
+  },
+  {
+    "productoID":3,
+    "Modelo":'RIO',
+    "Descripcion":"4 Puertas familiar",
+    "Year":"Agosto 3 2022",
+    "Precio":60000,
+    "Marca":"KIA",
+    "Color":"Azul",
+    "imagenUrl":"https://img.remediosdigitales.com/ee96cd/kia-rio-2021-mexico-precio_/1366_2000.jpg" 
+  } 
+]
+
+}
